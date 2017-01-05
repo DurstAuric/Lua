cdb5cc6e2e1a3fc88d29c00d9787358e6dfafe00
DurstAuric <DurstAuric@hotmail.com>
2017-01-05 14:12:44 +0000

    Create Settings.lua

---
+++ Discord Webhook/Settings.lua
@@ -0,0 +1,7 @@
+-- To learn how to use this, please read this: https://forum.roblox.com/Forum/ShowPost.aspx?PostID=204035494
+-- Make sure HttpService is enabled, or this will not work!
+local Settings = {
+Link = "", -- Your Discord Webhook link. Will not work without this.
+CharacterLimit = 250, -- How many characters may be inputted into the main textbox. Discord's limit is 2000.
+}
+return Settings
